1785325e84885d65452f23446665ae12362a13e8
YahyaAhmedKhan <yahyaahmed.52@gmail.com>
2023-04-03 00:05:32 +0500

    model tells class name now as well

--- api/routes/predict.js
+++ api/routes/predict.js
@@ -6,65 +6,67 @@ const fs = require("fs");
 const tfHub = require("@tensorflow/tfjs-converter");
 const tfn = require("@tensorflow/tfjs-node");
 
-// class myLayer extends tfHub.KerasLayer {
+const class_idx_map = {
+  Apple___Apple_scab: 0,
+  Apple___Black_rot: 1,
+  Apple___Cedar_apple_rust: 2,
+  Apple___healthy: 3,
+  Blueberry___healthy: 4,
+  "Cherry_(including_sour)___Powdery_mildew": 5,
+  "Cherry_(including_sour)___healthy": 6,
+  "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot": 7,
+  "Corn_(maize)___Common_rust_": 8,
+  "Corn_(maize)___Northern_Leaf_Blight": 9,
+  "Corn_(maize)___healthy": 10,
+  Grape___Black_rot: 11,
+  "Grape___Esca_(Black_Measles)": 12,
+  "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)": 13,
+  Grape___healthy: 14,
+  "Orange___Haunglongbing_(Citrus_greening)": 15,
+  Peach___Bacterial_spot: 16,
+  Peach___healthy: 17,
+  "Pepper,_bell___Bacterial_spot": 18,
+  "Pepper,_bell___healthy": 19,
+  Potato___Early_blight: 20,
+  Potato___Late_blight: 21,
+  Potato___healthy: 22,
+  Raspberry___healthy: 23,
+  Soybean___healthy: 24,
+  Squash___Powdery_mildew: 25,
+  Strawberry___Leaf_scorch: 26,
+  Strawberry___healthy: 27,
+  Tomato___Bacterial_spot: 28,
+  Tomato___Early_blight: 29,
+  Tomato___Late_blight: 30,
+  Tomato___Leaf_Mold: 31,
+  Tomato___Septoria_leaf_spot: 32,
+  "Tomato___Spider_mites Two-spotted_spider_mite": 33,
+  Tomato___Target_Spot: 34,
+  Tomato___Tomato_Yellow_Leaf_Curl_Virus: 35,
+  Tomato___Tomato_mosaic_virus: 36,
+  Tomato___healthy: 37,
+};
 
-//     static className = 'myLayer';
+const idx_class_map = {};
+for (let key in class_idx_map) {
+  let value = class_idx_map[key];
+  idx_class_map[value] = key;
+}
 
-//     constructor(config){
-//         super(config);
-//     }
-// }
-
-// // tf.serialization.registerClass(myLayer);
-
-// async function loadModel() {
-//   const hubUrl =
-//     "https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_050_224/classification/3/default/1";
-//   await tf.loadGraphModel(hubUrl, { fromTFHub: true });
-//   tf.engine().registerBackend(
-//     "tfjs",
-//     () => new tf.WebGLBackend(tf.getBackend())
-//   );
-//   tf.setBackend("tfjs");
-
-//   const handler = tfn.io.fileSystem(
-//     "/Users/yahyaahmedkhan/Desktop/dev/UniProjects/plants-ai-backend/tfjs_model/model.json"
-//   );
-
-//   const customLayers = { KerasLayer: tfHub.KerasLayer }; // uncomment and define customLayers
-
-//   const model = await tf.loadLayersModel(handler, {
-//     customObjects: customLayers,
-//   });
-
-//   const img = fs.readFileSync(
-//     "file:///Users/yahyaahmedkhan/Desktop/dev/UniProjects/plants-ai-backend/test_pics/images-3.jpeg"
-//   );
-
-//   const tensor = tf.node.decodeImage(img);
-
-//   const resized = tf.image.resizeBilinear(tensor, [224, 224]);
-//   const casted = resized.cast("float32");
-//   const expanded = casted.expandDims(0);
-//   const normalized = expanded.div(255.0);
-
-//   const prediction = model.predict(normalized);
-
-//   const probs = prediction.dataSync();
-//   const classIndex = probs.indexOf(Math.max(...probs));
-//   console.log("Predicted class index:", classIndex);
-// }
-
-// loadModel();
+let model;
 
 async function loadGraphModel() {
   const handler = tfn.io.fileSystem("tfjs_model_dir/model.json");
 
-  const model = await tf.loadGraphModel(handler);
+  model = await tf.loadGraphModel(handler);
+  // outputTensor = model.outputs[0];
+  // console.log(outputTensor.classNames); // ["class 0", "class 1", "class 2", ...]
+  return model;
+}
 
-  const img = fs.readFileSync(
-    "test_pics/images-3.jpeg"
-  );
+async function predict_image(img_path) {
+  // const img = fs.readFileSync("test_pics/images-3.jpeg");
+  const img = fs.readFileSync(img_path);
 
   const tensor = tfn.node.decodeImage(img);
 
@@ -73,14 +75,29 @@ async function loadGraphModel() {
   const expanded = casted.expandDims(0);
   const normalized = expanded.div(255.0);
 
+  if (!model) {
+    // Check if the model has already been loaded
+    await loadGraphModel(); // Load the model if it hasn't been loaded yet
+  }
   const prediction = model.predict(normalized);
 
   const probs = prediction.dataSync();
   const classIndex = probs.indexOf(Math.max(...probs));
-  console.log("Predicted class index:", classIndex);
+
+  // console.log("Predicted class index:", classIndex);
+  // console.log("Class: ", idx_class_map[classIndex])
+  // console.log("Accuracy: ", probs[classIndex])
+
+  return [idx_class_map[classIndex], probs[classIndex]];
 }
 
 loadGraphModel();
+
+predict_image("test_pics/apple-cedar-rust.jpeg").then(
+  result => {
+    console.log(result)
+  }
+)
 
 router.get("/", (req, res, next) => {
   // testing if taking requests
